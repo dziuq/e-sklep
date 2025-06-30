ec750a1e3c7a410a4b1392f5bfe9cf9fdb961070
PC\HomePC <dawid.dziuk@wp.pl>
2025-06-30 15:03:32 +0200

    Added categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./components/Directory/directory.component";
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Znaki ostrzegawcze",
+      imageSrc: "/images/ostrzegawcze.png",
+    },
+    {
+      id: 2,
+      title: "Znaki zakazu",
+      imageSrc: "/images/zakazu.png",
+    },
+    {
+      id: 3,
+      title: "Znaki nakazu",
+      imageSrc: "/images/nakazu.png",
+    },
+    {
+      id: 4,
+      title: "Znaki informacyjne",
+      imageSrc: "/images/informacyjne.png",
+    },
+    {
+      id: 5,
+      title: "Znaki kierunku i miejscowości",
+      imageSrc: "/images/kierunku_i_miejscowosci.png",
+    },
+    {
+      id: 6,
+      title: "Znaki uzupełniające",
+      imageSrc: "/images/uzupelniajace.png",
+    },
+    {
+      id: 7,
+      title: "Tabliczki do znaków drogowych",
+      imageSrc: "/images/tabliczki.png",
+    },
+    {
+      id: 8,
+      title: "Znaki kolejowe",
+      imageSrc: "/images/kolejowe.jpg",
+    },
+    {
+      id: 9,
+      title: "Znaki dodatkowe",
+      imageSrc: "/images/dodatkowe.png",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory categories={categories}/>
   );
-}
+};
 
 export default App;
